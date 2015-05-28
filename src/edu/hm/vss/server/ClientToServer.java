791a33221f04cc9c98b906e577b17713a600e134
Joncn <jonas.ringler@gmx.de>
2015-05-28 15:20:23 +0200

    Merge branch 'master' of https://github.com/TeMeise/rmi_philosopher
    Conflicts:
    	src/edu/hm/vss/server/ClientToServer.java

--- src/edu/hm/vss/server/ClientToServer.java
+++ src/edu/hm/vss/server/ClientToServer.java
@@ -75,7 +75,7 @@ public class ClientToServer implements IClientToServer
 
             Fork leftFork;
 
-            if(startIndex > 0 && server.getPlates().get(startIndex + i - 1).getRightFork() != null && server.getPlates().get(startIndex + i - 1).getRightFork() instanceof LocalFork)
+            if(startIndex > 0 && server.getPlates().get(startIndex + i - 1) != null && server.getPlates().get(startIndex + i - 1).getRightFork() instanceof LocalFork)
             {
                 leftFork = server.getPlates().get(startIndex +i - 1).getRightFork();
             }
